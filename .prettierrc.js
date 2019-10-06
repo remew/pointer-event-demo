528376f92604b2338de091db0e6fbf0864ac15ae
remew <foolishremew@gmail.com>
2019-10-06 14:51:23 +0900

    init project

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  bracketSpacing: false,
+  endOfLine: 'lf',
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'es5',
+};
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+render(
+  <>
+    <h1>Hello</h1>
+    <div>This is div</div>
+  </>,
+  document.getElementById('root')
+);
